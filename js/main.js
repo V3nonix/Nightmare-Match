772069f70a7cdf3c5735fc90655c78a16641f4e4
V3nonix <liam.grubbs@outlook.com>
2023-03-03 18:24:48 -0800

    Pseudocode frame completed.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,99 @@
 // Test log to confirm connection with server.
 
-    console.log('Live server test.');
+console.log('Live server test.');
 
-// Lesson work & notes:
+// Memory Match:
+
+    /*----- Constants -----*/
+    const CARD_TYPE = {
+        'a': 'crd-type-1',
+        'b': 'crd-type-2',
+        'c': 'crd-type-3',
+        'd': 'crd-type-4',
+        'e': 'crd-type-5',
+        'f': 'crd-type-6',
+        'g': 'crd-type-7',
+        'h': 'crd-type-8',
+        'i': 'crd-type-9',
+        'j': 'crd-type-10',
+        'k': 'crd-type-11',
+        'l': 'crd-type-12',
+        'm': 'crd-type-13',
+        'n': 'crd-type-14',
+        'o': 'crd-type-15',
+        'p': 'crd-type-16',
+        'q': 'crd-type-17',
+        'r': 'crd-type-18',      
+    };
+
+    /*----- State variables -----*/
+    // Array of rows on the card grid used to determine if a card is visible or not.
+    let crdGridVis;
+    // Array of rows on the card grid used to store card type.
+    let crdGrid;
+    // String which stores win/loss and the 
+    let rslt;
+
+    /*----- Cached elements  -----*/
+
+
+    /*----- Event listeners -----*/
+
+
+    /*----- Functions -----*/
+
+    
+
+    // Initialization functions:
+
+        /* init() auxiliary function. */
+
+
+        /* init() helper functions. */
+        function crdRandomizer() {
+            
+        }
+
+        /* init() function. */
+        function init() {
+
+        }
+
+    // Initialization: 
+
+    init();
+
+    // State Change functions:
+
+    // State Check functions:
+
+        /* winCheck() auxiliary function. */
+
+        /* winCheck() helper functions. */
+
+        /* winCheck() function. */
+        function winCheck() {
+
+        }
+
+        /* timer() function. */
+ 
+        /* lossCheck() auxiliary function. */
+
+        /* lossCheck() helper functions. */
+
+        /* lossCheck() function. */
+        
+    // Render functions:
+
+        /* render() helper functions. */
+        
+
+        /* render() function. */
+        function render() {
+
+        }
+
+
+
 
