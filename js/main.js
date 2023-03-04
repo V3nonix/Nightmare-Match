ab4f913151258555f65041c2e94fa8ec542e3843
V3nonix <liam.grubbs@outlook.com>
2023-03-04 14:34:28 -0800

    Pseudocode gameStart() logic completed.

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ console.log('Live server test.');
 // Memory Match:
 
     /*----- Constants -----*/
+    // Contains the reference value and class name key value pairs:
     const CARD_TYPE = {
         'a': 'crd-type-1',
         'b': 'crd-type-2',
@@ -19,60 +20,63 @@ console.log('Live server test.');
         'k': 'crd-type-11',
         'l': 'crd-type-12',     
     };
-
+    // Contains the keys for the class names stored as values in the object above:
     const CARD_REF = ['a','b','c','d','e','f','g','h','i','j','k','l'];
+    // Contains the max number of guess attempts available to the player:
+    const MAX_GUESS = 10;
     
     /*----- State variables -----*/
-    // Array of rows on the card grid used to determine if a card is visible or not.
+    // Array of rows on the card grid used to determine if a card is face-up or not.
     let crdGridVis;
     // Array of rows on the card grid used to store card type.
     let crdGrid;
+    // Number which stores max guesses.
+    let remGuess;
     // String which stores win/loss.
     let rslt;
+    // Boolean which stores if game has started yet.
+    let didGmStrt;
   
 
     /*----- Cached elements  -----*/
-
+    // Card grid elements:
+    // Restart button:
+    // Timer element:
+    // (Stretch goal) Animated element(s):
 
     /*----- Event listeners -----*/
-
+    // Card grid elements event listener:
+    // Restart button event listener:
+    // Timer element event listener:
 
     /*----- Functions -----*/
-
-    
 
     // Initialization functions:
 
         /* init() auxiliary functions. */
-
+        function bttrRandom() {
+            // Create a better random function so that there are less matches placed in a row.
+        }
 
         /* init() helper functions. */
 
             // crdRandomizer() helper functions:
             function spliceToFourRows(arr) {
-                /* Integer determining number of values in a row: */
-                let n = 6;
+                /* --REPLACE THIS WITH: CARD_REF.length/4-- Integer determining number of values in a row: */
+                let nPerRow = 6;
                 /* Array of arrays resulting from function: */
                 let arrRes;
                 /* Array representing the first row: */
-                const arrRowOne = arr.splice(-n);
+                const arrRowOne = arr.splice(-nPerRow);
                 /* Array representing the second row: */
-                const arrRowTwo = arr.splice(-n);
+                const arrRowTwo = arr.splice(-nPerRow);
                 /* Array representing the third row: */
-                const arrRowThree = arr.splice(-n);
+                const arrRowThree = arr.splice(-nPerRow);
                 /* Array representing the fourth row: */
-                const arrRowFour = arr.splice(-n);
+                const arrRowFour = arr.splice(-nPerRow);
+                /* Sets result array to an array of four rows then returns result array: */
                 arrRes = [[...arrRowOne], [...arrRowTwo], [...arrRowThree], [...arrRowFour]];
                 return arrRes;
-            }
-
-            function randomVal(n, arr) {
-                /* Sets a variable to the value corresponding with the random index: */
-                let val = arr[idx];
-                /* Sets global reference value to that of 'val': */
-                glblVal = val;
-                /* Returns 'val': */
-                return val;
             }
 
             // crdRandomizer() function:
@@ -86,13 +90,14 @@ console.log('Live server test.');
                 arrCrdRef = [...CARD_REF];
                 /* Looping randomizer set for as long there are less than 2 duplicates of each of the 12 cards: */
                 while (arrCrdRes.length < CARD_REF.length*2) {
-                    // Calculates a random index for array of n length:
+                    // --REPLACE THIS WITH: bttrRandom()-- Calculates a random index for array of n length: 
                     idx = Math.floor(Math.random()*n);
                     // Sets a variable to the value corresponding with the random index:
                     let val = arrCrdRef[idx];
                     // Determines behavior of adding new values to result array:
                     if (arrCrdRes.includes(val)) {
-                        arrCrdRes.pop(val);
+                        /* Sends duplicate to beginning of array rather than end to decrease number of adjacent matches: */
+                        arrCrdRes.unshift(val);
                         /* Removes the present 'val' from reference array so that it can not be selected again: */
                         arrCrdRef.splice(idx,1);
                         /* Subtracts 1 from n for new array lenght: */
@@ -104,30 +109,73 @@ console.log('Live server test.');
                 }
                 return spliceToFourRows(arrCrdRes)
             }
+
+            // gameStart() function:
+            function gameStart() {
+                /* --REMOVE ON FINAL VERSION-- Game start tests and logs: */
+                console.log('Before timeout was intended to occur.');
+                console.log(crdGridVis);
+                setTimeout(() => {
+                    // Sets all of the cards on the grid to face-down:
+                    crdGridVis = [
+                        [0,0,0,0,0,0],  // <---[Row 1]
+                        [0,0,0,0,0,0],  // <---[Row 2]
+                        [0,0,0,0,0,0],  // <---[Row 3]
+                        [0,0,0,0,0,0],  // <---[Row 4]
+                    ];
+                    // Set remaining guesses to max guesses:
+                    remGuess = MAX_GUESS;
+                    // Sets timer:
+                    timer();
+                    // Sets game start variable to true:
+                    didGmStrt = true;
+                    // Set message text to "GAME START!" :
+                        /* --NOT IMPLEMENTED AT THIS TIME-- */
+                    // Re-renders game state while cards are face down:
+                    render();
+                    /* --REMOVE ON FINAL VERSION-- Game start tests and logs: */
+                    console.log('After timeout was intended to occur.');
+                    console.log(crdGridVis);
+                }, 3000);
+            }
             
         /* init() function. */
         function init() {
-            // Sets all of the cards on the grid to hidden:
-            crdGridVis = [
-                [0,0,0,0,0,0],  // <---[Row 1]
-                [0,0,0,0,0,0],  // <---[Row 2]
-                [0,0,0,0,0,0],  // <---[Row 3]
-                [0,0,0,0,0,0],  // <---[Row 4]
-            ];
-            // Sets the card grid to a random placement of 2 duplicates of each of the 12 cards:
+            // Sets the card grid to a random placement of 24 cards containing 12 matches:
             crdGrid = crdRandomizer();
+            // Starts all of the cards on the grid to face-up:
+            crdGridVis = [
+                [1,1,1,1,1,1],  // <---[Row 1]
+                [1,1,1,1,1,1],  // <---[Row 2]
+                [1,1,1,1,1,1],  // <---[Row 3]
+                [1,1,1,1,1,1],  // <---[Row 4]
+            ];
+            // Sets results to null:
+            rslt = null;
+            // Sets remaining guesses to 'locked' while all cards are face-up:
+            remGuess = 'locked';
+            // Renders game state while cards are face-up:
+            render();
+            // Calls game start function to start the game:
+            gameStart();
+            
         }
 
     // Initialization: 
 
     init();
 
-    console.log(crdGrid);
+        /* --REMOVE ON FINAL VERSION-- After-initialization tests and logs: */
+        console.log(crdGrid);
+
     // State Change functions:
 
     // State Check functions:
 
         /* winCheck() auxiliary function. */
+        function checkMatch() {
+
+        }
 
         /* winCheck() helper functions. */
 
@@ -137,6 +185,9 @@ console.log('Live server test.');
         }
 
         /* timer() function. */
+        function timer() {
+
+        }
  
         /* lossCheck() auxiliary function. */
 
@@ -147,18 +198,28 @@ console.log('Live server test.');
     // Render functions:
 
         /* render() helper functions. */
-
-        /* render() function. */
-        function render() {
+        function renderCrdGrid() {
 
         }
 
+        function renderMsg() {
+
+        }
+
+        function renderCtrls() {
+
+        }
+
+        function renderTmr() {
+
+        }
+
+        /* render() function. */
+        function render() {
+            renderCrdGrid();
+            renderMsg();
+            renderCtrls();
+            renderTmr();
+        }
 
 // Notes:
-
-
-
-
-
-
-
