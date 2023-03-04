803937c79e04448a7ec6225956ca9a3f892c66cc
V3nonix <liam.grubbs@outlook.com>
2023-03-04 01:25:46 -0800

    Pseudocode crdRandomizer() logic completed.

--- js/main.js
+++ js/main.js
@@ -17,22 +17,19 @@ console.log('Live server test.');
         'i': 'crd-type-9',
         'j': 'crd-type-10',
         'k': 'crd-type-11',
-        'l': 'crd-type-12',
-        'm': 'crd-type-13',
-        'n': 'crd-type-14',
-        'o': 'crd-type-15',
-        'p': 'crd-type-16',
-        'q': 'crd-type-17',
-        'r': 'crd-type-18',      
+        'l': 'crd-type-12',     
     };
 
+    const CARD_REF = ['a','b','c','d','e','f','g','h','i','j','k','l'];
+    
     /*----- State variables -----*/
     // Array of rows on the card grid used to determine if a card is visible or not.
     let crdGridVis;
     // Array of rows on the card grid used to store card type.
     let crdGrid;
-    // String which stores win/loss and the 
+    // String which stores win/loss.
     let rslt;
+  
 
     /*----- Cached elements  -----*/
 
@@ -46,23 +43,86 @@ console.log('Live server test.');
 
     // Initialization functions:
 
-        /* init() auxiliary function. */
+        /* init() auxiliary functions. */
 
 
         /* init() helper functions. */
-        function crdRandomizer() {
-            
-        }
 
+            // crdRandomizer() helper functions:
+            function spliceToFourRows(arr) {
+                /* Integer determining number of values in a row: */
+                let n = 6;
+                /* Array of arrays resulting from function: */
+                let arrRes;
+                /* Array representing the first row: */
+                const arrRowOne = arr.splice(-n);
+                /* Array representing the second row: */
+                const arrRowTwo = arr.splice(-n);
+                /* Array representing the third row: */
+                const arrRowThree = arr.splice(-n);
+                /* Array representing the fourth row: */
+                const arrRowFour = arr.splice(-n);
+                arrRes = [[...arrRowOne], [...arrRowTwo], [...arrRowThree], [...arrRowFour]];
+                return arrRes;
+            }
+
+            function randomVal(n, arr) {
+                /* Sets a variable to the value corresponding with the random index: */
+                let val = arr[idx];
+                /* Sets global reference value to that of 'val': */
+                glblVal = val;
+                /* Returns 'val': */
+                return val;
+            }
+
+            // crdRandomizer() function:
+            function crdRandomizer() {
+                /* Integer determing possible outputs for randomizer: */
+                let n = CARD_REF.length-1;
+                /* Array resulting from randomizer: */
+                let arrCrdRes = [];
+                /* Array created from reference array and altered by randomizer: */
+                let arrCrdRef;
+                arrCrdRef = [...CARD_REF];
+                /* Looping randomizer set for as long there are less than 2 duplicates of each of the 12 cards: */
+                while (arrCrdRes.length < CARD_REF.length*2) {
+                    // Calculates a random index for array of n length:
+                    idx = Math.floor(Math.random()*n);
+                    // Sets a variable to the value corresponding with the random index:
+                    let val = arrCrdRef[idx];
+                    // Determines behavior of adding new values to result array:
+                    if (arrCrdRes.includes(val)) {
+                        arrCrdRes.pop(val);
+                        /* Removes the present 'val' from reference array so that it can not be selected again: */
+                        arrCrdRef.splice(idx,1);
+                        /* Subtracts 1 from n for new array lenght: */
+                        n -= 1;
+                    } else {
+                        /* If there is not yet a matching value, adds to the end of array: */
+                        arrCrdRes.push(val)
+                    };
+                }
+                return spliceToFourRows(arrCrdRes)
+            }
+            
         /* init() function. */
         function init() {
-
+            // Sets all of the cards on the grid to hidden:
+            crdGridVis = [
+                [0,0,0,0,0,0],  // <---[Row 1]
+                [0,0,0,0,0,0],  // <---[Row 2]
+                [0,0,0,0,0,0],  // <---[Row 3]
+                [0,0,0,0,0,0],  // <---[Row 4]
+            ];
+            // Sets the card grid to a random placement of 2 duplicates of each of the 12 cards:
+            crdGrid = crdRandomizer();
         }
 
     // Initialization: 
 
     init();
 
+    console.log(crdGrid);
     // State Change functions:
 
     // State Check functions:
@@ -87,12 +147,17 @@ console.log('Live server test.');
     // Render functions:
 
         /* render() helper functions. */
-        
 
         /* render() function. */
         function render() {
 
         }
+
+
+// Notes:
+
+
+
 
 
 
